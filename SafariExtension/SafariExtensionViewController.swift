e3827fae4ba6d606babddea0a07a744318aff0df
iAugus <iAugux@gmail.com>
2016-06-29 13:10:30 +0800

    init

---
+++ SafariExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  SafariExtension
+//
+//  Created by Augus on 6/29/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
